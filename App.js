c7dc0304c589fe602106e1dc33a4051499ca2900
Bhavuk Jain <bhavuk_92@yahoo.co.in>
2018-02-23 16:57:58 +0530

    Added tabbar icons.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation'
 import DeckList from './components/DeckList'
 import NewDeck from './components/NewDeck'
+import {FontAwesome, Ionicons} from '@expo/vector-icons'
 
 export default class App extends React.Component {
   render() {
@@ -18,23 +19,30 @@ const Tabs = TabNavigator({
   Decks: {
     screen: DeckList,
     navigationOptions: {
-      tabBarLabel:'Decks'
+      tabBarLabel:'Decks',
+      tabBarIcon: (({ focused, tintColor }) => <FontAwesome name='home' size={25} color={tintColor}></FontAwesome>)
     }
   },
 
   NewDeck: {
     screen: NewDeck,
     navigationOptions: {
-      tabBarLabel:'New Deck'
+      tabBarLabel:'New Deck',
+      tabBarIcon: (({ focused, tintColor }) => <Ionicons name='md-add' size={25} color={tintColor}></Ionicons>)
     }
-  },
+  }}, {
+
+  tabBarOptions: {
+    activeTintColor: 'black',
+    inactiveTintColor:'gray'
+  }
 
 })
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -5,14 +5,39 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 class NewDeck extends Component {
+
+
+  state = {text:''}
+
+
+  onPressLearnMore = () => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-		<Text>New Deck</Text>
+		<Text style={styles.text}>What is the name of your new deck card?</Text>
+		<View style={styles.textInputView}>
+			<TextInput
+	        	style={{margin:10}}
+	        	placeholder={'Enter the deck name'}
+	        	onChangeText={(text) => this.setState({text})}
+	        	value={this.state.text}
+	      	/>
+		</View>
+		<TouchableOpacity
+		   style={styles.button}
+  		   onPress={this.onPressLearnMore}
+		>
+		<Text style={{color:'white'}}>Add Deck</Text>
+		</TouchableOpacity>
       </View>
     );
   }
@@ -21,10 +46,29 @@ class NewDeck extends Component {
 const styles = StyleSheet.create({
 container: {
     flex: 1,
-    backgroundColor: '#fafafa',
-    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    margin: 10
   },
+  text: {
+	alignSelf:'center',
+  },
+  textInputView: {
+  	height: 35,
+  	borderColor: 'gray',
+  	borderWidth: 1,
+  	margin: 20,
+  },
+  button:{
+  	alignSelf:'center',
+  	margin: 20,
+  	backgroundColor:"gray",
+  	height: 40,
+  	width: 90,
+  	justifyContent:'center',
+  	alignItems:'center'
+  }
 });
 
 
